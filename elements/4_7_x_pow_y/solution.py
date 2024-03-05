a8e8a79e236b8c7c2591d5318eeca9205e5a43c9
ashwin-nair98 <ashwin.bala.432@gmail.com>
2024-03-05 21:55:59 +0530

    feat: add x to the power y

--- elements/4_7_x_pow_y/solution.py
+++ elements/4_7_x_pow_y/solution.py
@@ -1,1 +1,25 @@
 # Question: Find x^y
+
+# x^y = x2^y/2 or x * x2^y/2
+def power(x, y):
+    result = 1.0
+    power = y
+    while power:
+        if power & 1:
+            result *= x
+        x, power = x * x, power >> 1
+    return result
+
+
+# Test
+def expect(a, b):
+    result = a == b
+    print("Test for " + str(a) + " : " + (" passed!" if result else " failed!"))
+
+
+x, y = 2, 3
+expect(8, power(x, y))
+x, y = 3, 3
+expect(27, power(x, y))
+x, y = 4, 3
+expect(64, power(x, y))
